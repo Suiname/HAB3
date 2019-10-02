4da1d429a0d8f75371f9cc9b15fa2ba159726ab6
Jason Tham <jason@moonrisers.com>
2019-10-02 15:41:34 -0500

    Apollo Server GQL added to project: - Added apollo gql server docker - Updated docker configs - Installed and configure sequelize - Created basic dummy user schema migration - Created basic users query, schema and resolvers for gql

---
+++ gql/types/index.js
@@ -0,0 +1,7 @@
+const users = require('./users');
+
+const typeDefs = {
+	...users,
+}
+
+module.exports = typeDefs;
---
+++ gql/types/users.js
@@ -0,0 +1,23 @@
+const { gql } = require('apollo-server');
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = gql`
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type User {
+	id: ID
+    username: String
+    password: String
+    createdAt: String
+    updatedAt: String
+  }
+
+  type Query {
+    users: [User]
+  }
+`;
+
+module.exports = typeDefs;
